b65df93bb130ab37d012d18416a0eb8b35547ac7
dingguozhu <dgz888@163.com>
2019-06-05 01:11:39 +0800

    qa part2

--- src/main/java/edu/gzhu/its/qa/web/QaController.java
+++ src/main/java/edu/gzhu/its/qa/web/QaController.java
@@ -1,12 +1,16 @@
 package edu.gzhu.its.qa.web;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.jena.atlas.json.JsonObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.gson.JsonArray;
 
@@ -40,12 +44,63 @@ public class QaController {
 			JSONObject data = new JSONObject();
 			data.put("mine", mine);
 			
+
+		   /* //好友列表
+		    ,"friend": [{
+		      "groupname": "前端码屌" //好友分组名
+		      ,"id": 1 //分组ID
+		      ,"list": [{ //分组下的好友列表
+		        "username": "贤心" //好友昵称
+		        ,"id": "100001" //好友ID
+		        ,"avatar": "a.jpg" //好友头像
+		        ,"sign": "这些都是测试数据，实际使用请严格按照该格式返回" //好友签名
+		        ,"status": "online" //若值为offline代表离线，online或者不填为在线
+		      }, …… ]
+		    }, …… ]
+		    */
+			//朋友接口
+			JSONObject friendgroup1 = new JSONObject();
+			friendgroup1.put("groupname",  "中华传统文化问答机器人");
+			friendgroup1.put("id",  "1");
+			
+			JSONArray array = new JSONArray();
+			JSONObject hanzi = new JSONObject();
+			hanzi.put("username", "汉字大师");
+			hanzi.put("id", "1000");
+			hanzi.put("avatar", "2.jpg");
+			hanzi.put("sign", "3000个汉字你随便问");
+			hanzi.put("status", "online");
+			
+			JSONObject poetry = new JSONObject();
+			poetry.put("username", "古诗词大师");
+			poetry.put("id", "1001");
+			poetry.put("avatar", "2.jpg");
+			poetry.put("sign", "唐诗宋词你随便问");
+			poetry.put("status", "online");
+			array.add(hanzi);
+			array.add(poetry);
+			friendgroup1.put("list", array);
+			
+			JSONArray friendgroups = new JSONArray();
+			friendgroups.add(friendgroup1);
+			data.put("friend", friendgroups);
 			map.put("data",data);
 
 		}catch(Exception e){
 			map.put("code", 1);
 			map.put("msg", "存在异常！");
 		}
+		return map;
+	}
+	
+	
+	@GetMapping("/qa/question")
+	@ResponseBody
+	public Map<String,Object> question(String sendId,String content,String toId){
+		System.out.println(sendId);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("answer", content +"回答");
+		map.put("timestamp", new java.sql.Timestamp(new Date().getTime()));
 		return map;
 	}
 	
--- src/main/webapp/WEB-INF/views/qa/index.jsp
+++ src/main/webapp/WEB-INF/views/qa/index.jsp
@@ -14,6 +14,7 @@
 	-->
 <link rel="stylesheet" href="/layim/css/layui.css">
 <script src="/layim/layui.js"></script>
+<script src="/static/lib/jquery/1.9.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -28,40 +29,45 @@
 					type : 'get', //默认get，一般可不填
 					data : {} //额外参数
 				}
-			}).chat({
-				name : '客服姐姐',
-				type : 'friend',
-				avatar : 'https://cdn.layui.com/avatar/168.jpg?t=1507143496817',
-				id : 2000
 			});
-			
-			layim.on('sendMessage', function(res){
-  var mine = res.mine; //包含我发送的消息及我的信息
-  console.log(mine);
-  obj = {
-          username: "客服姐姐"
-          ,avatar: mine.avatar
-          ,id: 2000
-          ,type: mine.type
-          ,content: "1234"
-        }
-  layim.getMessage(obj);
-  	
-  });
-		
-		
-		layim.getMessage({
-  username: "纸飞机" //消息来源用户名
-  ,avatar: "http://tp1.sinaimg.cn/1571889140/180/40030060651/1" //消息来源用户头像
-  ,id: "100000" //消息的来源ID（如果是私聊，则是用户id，如果是群聊，则是群组id）
-  ,type: "friend" //聊天窗口来源类型，从发送消息传递的to里面获取
-  ,content: "嗨，你好！本消息系离线消息。" //消息内容
-  ,cid: 0 //消息id，可不传。除非你要对消息进行一些操作（如撤回）
-  ,mine: false //是否我发送的消息，如果为true，则会显示在右方
-  ,fromid: "100000" //消息的发送者id（比如群组中的某个消息发送者），可用于自动解决浏览器多窗口时的一些问题
-  ,timestamp: 1467475443306 //服务端时间戳毫秒数。注意：如果你返回的是标准的 unix 时间戳，记得要 *1000
-});
-	});	
+	
+			layim.on('sendMessage', function(res) {
+				var mine = res.mine;
+				var sendId = mine.id;
+				var content = mine.content;
+				var to = res.to;
+				var toId = to.id;
+				$.ajax({
+					type : 'get',
+					url : '/qa/question',
+					data : {
+						"sendId" : sendId,
+						"content" : content,
+						"toId" : toId
+					},
+					success : function(result) {
+						layim.getMessage({
+							username : res.to.username, //消息来源用户名
+							avatar : res.to.avatar, //消息来源用户头像
+							id : sendId, //消息的来源ID（如果是私聊，则是用户id，如果是群聊，则是群组id）
+							type : "friend", //聊天窗口来源类型，从发送消息传递的to里面获取
+							content : result.answer, //消息内容
+							mine : false, //是否我发送的消息，如果为true，则会显示在右方
+							fromid : toId, //消息的发送者id（比如群组中的某个消息发送者），可用于自动解决浏览器多窗口时的一些问题
+							timestamp : result.timestamp //服务端时间戳毫秒数。注意：如果你返回的是标准的 unix 时间戳，记得要 *1000
+						});
+					},
+					error : function(msg) {
+						alert("failed:" + JSON.stringify(msg));
+					},
+					dataType : "json"
+				});
+	
+			});
+		});
+		function success() {
+			alert();
+		}
 	</script>
 </body>
 </html>
